26586f1bc428d99941756f34df6ffb0c5cd4c0f4
Alyssa Moody and david rolfs <alyssa+david@>
2017-05-03 13:55:26 -0700

    work out bugs

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var vowels = ["a", "e", "i", "o", "u"];
 var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 
 
-var toPigLatin = function(word){
+function toPigLatin(word){
   if(word.length <= 1){
     if (word[0]==="a"|| word[0]==="i"){
       return word[0]+"way";
@@ -12,14 +12,13 @@ var toPigLatin = function(word){
       for(var i=0; i<= consonants.length; i++){
         if(word[0]===vowels[i]){
           return word+"ay";
-
-        }else if((word[1]&&word[0])=== consonants[i]){
+        }else if(word[1] === consonants[i]){
           alert("works");
           var twoConsonant = word.replace(word[0],"").replace(word[1],"");
           return twoConsonant+word[0]+word[1]+"ay";
 
         }else if(word[0] === consonants[i]){
-          alert("um workss");
+          //alert("um workss");
           var noConsonant = word.replace(word[0],"");
           return noConsonant+word[0]+"ay";
         }else{
@@ -31,6 +30,14 @@ var toPigLatin = function(word){
       }
 }
 
+// for (var i = 0; i < word.length; i++) {
+//   if (is word[i] a consonant) {
+//     add to string StuffWereGOnnaMoveLater
+//     keep track of how long StuffWereGOnnaMoveLater is
+//   } else {
+//     break;
+//   }
+// }
 
 
 
