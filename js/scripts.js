67058b61dc304c30d09fd2bb9b4c52f69441133c
Alyssa Moody and david rolfs <alyssa+david@>
2017-05-03 15:41:55 -0700

    debugging...still.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //back-end
 var vowels = ["a", "e", "i", "o", "u"];
-var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 
 
 function toPigLatin(word){
@@ -9,26 +9,44 @@ function toPigLatin(word){
       return word[0]+"way";
     }
   } else if(word.length >= 2){
-      for(var i=0; i<= consonants.length; i++){
+      for(var i=0; i<= vowels.length; i++){
         if(word[0]===vowels[i]){
           return word+"ay";
-        }else if(word[1] === consonants[i]){
-          alert("works");
-          var twoConsonant = word.replace(word[0],"").replace(word[1],"");
-          return twoConsonant+word[0]+word[1]+"ay";
-
-        }else if(word[0] === consonants[i]){
-          //alert("um workss");
-          var noConsonant = word.replace(word[0],"");
-          return noConsonant+word[0]+"ay";
-        }else{
-          //alert("doesnt begin with vowel")
         }
       }
-    } else{
-      //alert("not a word")
+      for(var j=0; j<= consonants.length; j++){
+        if(word[0,1,2]===consonants[j]){
+          var threeConsonant = word.replace(word[0],"").replace(word[1],"").replace(word[2],"");
+          return threeConsonant+word[0]+word[1]+word[2]+"ay";
+        } else if (word[0,1]===consonants[j]){
+          var twoConsonant = word.replace(word[0],"").replace(word[1],"");
+          return twoConsonant+word[0]+word[1]+"ay";
+        } else if (word[0]===consonants[j]){
+          var oneConsonant = word.replace(word[0],"");
+          return oneConsonant+word[0]+"ay";
+        }
       }
-}
+    }
+  }
+      // }else if(word[0] === consonants[i]){
+      //     if(word[1] === consonants[i]){
+            // var twoConsonant = word.replace(word[0],"").replace(word[1],"");
+            // return twoConsonant+word[0]+word[1]+"ay";
+    //       }else if(word[0] === consonants[i]){
+    //         //alert("um workss");
+    //         var noConsonant = word.replace(word[0],"");
+    //         return noConsonant+word[0]+"ay";
+    //       }else{
+    //
+    //         }
+    //     }else{
+    //       //alert("doesnt begin with vowel")
+    //     }
+    //   }
+    // } else{
+    //   //alert("not a word")
+
+
 
 // for (var i = 0; i < word.length; i++) {
 //   if (is word[i] a consonant) {
